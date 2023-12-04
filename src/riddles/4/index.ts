93a89a1772884fb7ae64cbd6804086708d25698a
Konlanx <konlanx@pop-os.localdomain>
2023-12-04 15:44:40 +0100

    Optimized day 4, added timing

--- src/riddles/4/index.ts
+++ src/riddles/4/index.ts
@@ -3,7 +3,8 @@ import { getLines } from "../../utils"
 type Card = {
     id: number,
     winning: number[],
-    values: number[]
+    values: number[],
+    winningNumbers: number
 }
 
 const CARD = 'Card '
@@ -20,19 +21,20 @@ const parseCard = (line: string): Card => {
     const winning = parseValues(splitValues[0])
     const values = parseValues(splitValues[1])
 
+    const winningNumbers = values.filter(value => winning.find(win => value === win)).length
+
     return {
         id,
         winning,
-        values
+        values,
+        winningNumbers
     }
 }
 
 const parseCards = (lines: string[]): Card[] => lines.map(parseCard)
 
-const winningNumbers = (card: Card): number => card.values.filter(value => card.winning.find(win => value === win)).length
-
 const analyzeCards = (cards: Card[]): number => {
-    const matches: number[] = cards.reduce<number[]>((acc, curr) => [...acc, winningNumbers(curr)], []).filter(win => win)
+    const matches: number[] = cards.map(card => card.winningNumbers).filter(win => win)
 
     return matches.map(match => Math.pow(2, (match - 1))).reduce<number>((acc, curr) => acc += curr, 0)
 }
@@ -48,15 +50,18 @@ const multiplyCards = (cards: Card[], additionalCards: Card[]): Card[] => {
     const originalCards = [...cards]
 
     return additionalCards.reduce<Card[]>((acc, curr) => {
-        const winningNumbersAmount = winningNumbers(curr)
+        if (!curr.winningNumbers) return [...acc, curr]
 
-        if (!winningNumbersAmount) return [...acc, curr]
-
-        const newCards = originalCards.slice(curr.id, curr.id + winningNumbersAmount)
+        const newCards = originalCards.slice(curr.id, curr.id + curr.winningNumbers)
         const multipliedCards = multiplyCards(originalCards, newCards)
         return [...acc, curr, ...multipliedCards]
     },[])
 }
 
+const before = performance.now()
 const multipliedCards = multiplyCards(cards, cards)
+const after = performance.now()
+
+console.log(`Sum 2 took ${after - before} ms`)
+
 console.log('Sum 2', multipliedCards.length)
