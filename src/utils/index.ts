58f2107b7067bb2697aa21a18ca4f7e35ad8e574
Konlanx <konlanx@pop-os.localdomain>
2023-12-05 10:22:30 +0100

    Day 5 part 1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,8 +2,9 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export const getLines = (riddle: number): string[] => {
-    const content = fs.readFileSync(path.resolve(__dirname, `../../assets/riddles/${riddle}/input.txt`), 'utf8')
-    return content.split('\n')
+    return getContent(riddle).split('\n')
 }
+
+export const getContent = (riddle: number): string => fs.readFileSync(path.resolve(__dirname, `../../assets/riddles/${riddle}/input.txt`), 'utf8')
 
 export const isNumericChar = (character: string): boolean => /^\d+$/.test(character)
